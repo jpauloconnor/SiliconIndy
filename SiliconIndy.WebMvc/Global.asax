94d81f9ac69d77d186c011aff943a0b0af1aeacd
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 06:30:24 -0400

    Add project files.

---
+++ SiliconIndy.WebMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SiliconIndy.WebMvc.MvcApplication" Language="C#" %>
---
+++ SiliconIndy.WebMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SiliconIndy.WebMvc.Startup))]
+namespace SiliconIndy.WebMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
