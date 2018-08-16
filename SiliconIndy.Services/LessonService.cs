0c050100a0bfdb584b0dc084495fbefec82a9442
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 06:56:55 -0400

    Implemented interface on the LessonService

---
+++ SiliconIndy.Services/LessonService.cs
@@ -0,0 +1,38 @@
+﻿using SiliconIndy.Contracts;
+using SiliconIndy.Models.LessonModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconIndy.Services
+{
+    public class LessonService : ILessonService
+    {
+        public bool CreateLesson(LessonCreate model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteLesson(int lessonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LessonDetail GetLessonById(int lessonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LessonListItem> GetLessons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateLesson(LessonEdit model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
