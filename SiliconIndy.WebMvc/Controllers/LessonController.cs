2ecac671711424860434b00a48cd897ac89dc755
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 07:09:29 -0400

    MVC: Lesson controller methods, service helper, and views added

---
+++ SiliconIndy.WebMvc/Controllers/LessonController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.Identity;
+using SiliconIndy.Models.LessonModels;
+using SiliconIndy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconIndy.WebMvc.Controllers
+{
+    public class LessonController : Controller
+    {
+        // GET: Lesson
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LessonCreate lesson)
+        {
+            //TODO: Validation stuff
+            var lessonService = CreateLessonService();
+            lessonService.CreateLesson(lesson);
+
+            return RedirectToAction("Index");
+        }
+
+        public LessonService CreateLessonService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new LessonService(userId);
+            return service;
+        }
+    }
+}
