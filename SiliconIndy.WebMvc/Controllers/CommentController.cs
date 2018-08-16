0afd3ea25615c7250a4d2bb31c786819ad7c6baa
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 08:04:59 -0400

    MVC: Worked on CommentController Create methods

---
+++ SiliconIndy.WebMvc/Controllers/CommentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.Identity;
+using SiliconIndy.Models.CommentModels;
+using SiliconIndy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconIndy.WebMvc.Controllers
+{
+    [Authorize]
+    public class CommentController : Controller
+    {
+        public ActionResult Create(int lessonId)
+        {
+            var model = new CommentCreate
+            {
+                LessonId = lessonId,
+                UserId = Guid.Parse(User.Identity.GetUserId()) 
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CommentCreate comment)
+        {
+            var service = CreateCommentService(comment);
+
+            if (service.CreateComment(comment))
+            {
+                TempData["SaveResult"] = "Your comment was created.";
+                return RedirectToAction("Details", "Lesson", new { id = comment.LessonId });
+            }
+
+            ModelState.AddModelError("", "Comment could not be created");
+            return View(comment);
+        }
+        
+        private CommentService CreateCommentService(CommentCreate comment)
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new CommentService(userId, comment.LessonId);
+            return service;
+        }
+    }
+}
