fd12eb206637494e9e03dc6cfed1902bab2b3d41
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 07:29:28 -0400

    Contract: Added Comment interface/contract

---
+++ SiliconIndy.Contracts/ICommentService.cs
@@ -0,0 +1,19 @@
+﻿using SiliconIndy.Models.CommentModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconIndy.Contracts
+{
+    public interface ICommentService
+    {
+        bool CreateComment(CommentCreate model);
+        ICollection<CommentListItem> GetComments();
+        ICollection<CommentListItem> GetCommentsByLessonId(int lessonId);
+        CommentDetail GetCommentById(int lessonId);
+        bool UpdateComment(CommentEdit model);
+        bool DeleteComment(int lessonId);
+    }
+}
