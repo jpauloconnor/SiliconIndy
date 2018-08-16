2fe1934054501cc338b3c2eeaeda21237d5d20db
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 06:40:04 -0400

    Data layer: 1st model & identity models added

--- SiliconIndy.Data/Lesson.cs
+++ SiliconIndy.Data/Lesson.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SiliconIndy.Data
 {
-    public class Class1
+    public class Lesson
     {
+        [Key]
+        public int LessonId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset ModifiedUtc { get; set; }
     }
 }
