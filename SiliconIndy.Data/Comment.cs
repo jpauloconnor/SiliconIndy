e838e683c63331f06edcb51c03b14ebca680ab1a
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-16 07:17:30 -0400

    Data: Added comment data class and first migration

---
+++ SiliconIndy.Data/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconIndy.Data
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string CommentText { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
---
+++ SiliconIndy.Data/Migrations/201808161116550_comment.cs
@@ -0,0 +1,16 @@
+namespace SiliconIndy.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class comment : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
