09af89a8408ec877474d2bf945affeee676333bf
Paul O'Connor <jamespauloconnor@gmail.com>
2018-08-17 14:42:54 -0400

    View: Some alterations made in the home view. Placeholder images

--- SiliconIndy.WebMvc/App_Start/BundleConfig.cs
+++ SiliconIndy.WebMvc/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace SiliconIndy.WebMvc
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,17 +13,18 @@ namespace SiliconIndy.WebMvc
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/js/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/font-awesome.css",
+                      "~/Content/css/PagedList.css",
+                      "~/Content/css/styles.css",
+                      "~/Content/css/site.css"));
         }
     }
 }
