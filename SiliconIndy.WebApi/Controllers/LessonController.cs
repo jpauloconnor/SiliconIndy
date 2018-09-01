ddcdce32b7f4fa1eec4c41c1ab856411247e935e
Paul O'Connor <jamespauloconnor@gmail.com>
2018-09-01 15:29:54 -0400

    Huge overhaul of the UI

---
+++ SiliconIndy.WebApi/Controllers/LessonController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using SiliconIndy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SiliconIndy.WebApi.Controllers
+{
+    public class LessonController : ApiController
+    {
+        // GET /api/lessons
+        public IHttpActionResult Get()
+        {
+            var lessonService = CreateLessonService();
+
+            var lessons = lessonService.GetLessons();
+
+            return Ok(lessons);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+        private LessonService CreateLessonService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var lessonService = new LessonService(userId);
+            return lessonService;
+        }
+    }
+}
---
+++ SiliconIndy.WebMvc/Controllers/ResultsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiliconIndy.WebMvc.Controllers
+{
+    public class ResultsController : Controller
+    {
+        // GET: Charts
+        public ActionResult Individual()
+        {
+            return View();
+        }
+
+        public ActionResult AllUsers()
+        {
+            return View();
+        }
+    }
+}
